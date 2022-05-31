c40961b8b13d0965f446242f1211959eebf6ad5d
TeksPeks <53875005+TeksPeks@users.noreply.github.com>
2022-05-31 20:39:45 +0300

    refactor flip_image

--- flip_image.py
+++ flip_image.py
@@ -3,22 +3,21 @@ import os
 from .path_utils import get_res_path
 
 
-def flip_image(path: str, flip_mode: str) -> Image:
-    image = Image.open(path)
-
+def flip_image(raw_img: Image, flip_mode: str) -> Image:
     if flip_mode == 'vertical':
-        output = image.transpose(Image.FLIP_TOP_BOTTOM)
+        output = raw_img.transpose(Image.FLIP_TOP_BOTTOM)
     elif flip_mode == 'horizontal':
-        output = image.transpose(Image.FLIP_LEFT_RIGHT)
+        output = raw_img.transpose(Image.FLIP_LEFT_RIGHT)
     else:
-        temp = image.transpose(Image.FLIP_TOP_BOTTOM)
+        temp = raw_img.transpose(Image.FLIP_TOP_BOTTOM)
         output = temp.transpose(Image.FLIP_LEFT_RIGHT)
 
     return output
 
 
 def get_flipped_image(path: str, flip_mode: str) -> str:
-    flipped_img = flip_image(path, flip_mode)
+    raw_img = Image.open(path)
+    flipped_img = flip_image(raw_img, flip_mode)
 
     raw_img_name = os.path.basename(path)
     res_path = get_res_path(raw_img_name)
