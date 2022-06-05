83a609c030b26430717acb57a26930fabbba18c2
TeksPeks <53875005+TeksPeks@users.noreply.github.com>
2022-06-05 22:29:08 +0300

    lint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,74 +1,75 @@
-import FileUpload from './components/FileUpload'
-import downloadIcon from './assets/download.png'
-import './app.css'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import FileUpload from './components/FileUpload';
+import downloadIcon from './assets/download.png';
+import './app.css';
 
-const serverPath = 'https://myroslav-image-flip.herokuapp.com'
+const serverPath = 'https://myroslav-image-flip.herokuapp.com';
 
-function App() {
-  const [type, setType] = useState('');
-  const [resultLink, setResultLink] = useState('')
-  const [photo, setPhoto] = useState(null)
-  const [resultReady, setResultReady] = useState(false)
+function App () {
+  const [type, setType] = useState(null);
+  const [resultLink, setResultLink] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [resultReady, setResultReady] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const getReversedImage = () => {
     if (!photo) {
-      alert('choose image to flip')
+      alert('choose image to flip');
       return;
     }
     if (!type) {
-      alert('choose flip type')
+      alert('choose flip type');
       return;
     }
 
-    let formData = new FormData();
+    const formData = new FormData();
 
-    formData.append("photo", photo);
-    formData.append("flip_mode", type);
+    formData.append('photo', photo);
+    formData.append('flip_mode', type);
 
     setLoading(true);
 
-    fetch(`${serverPath}/upload`, {method: "POST", body: formData}).then(res => res.text()).then(filename => {
-        const newResLink = `${serverPath}/download/${filename}`;
-        setResultLink(newResLink);
-        const newPhoto = {...photo};
-        newPhoto.preview = newResLink;
-        newPhoto.name = filename;
-        setPhoto(newPhoto);
-        setLoading(false);
-        setResultReady(true);
-    })
-  }
+    fetch(`${serverPath}/upload`, { method: 'POST', body: formData }).then(res => res.text()).then(filename => {
+      const newResLink = `${serverPath}/download/${filename}`;
+      setResultLink(newResLink);
+      const newPhoto = { ...photo };
+      newPhoto.preview = newResLink;
+      newPhoto.name = filename;
+      setPhoto(newPhoto);
+      setLoading(false);
+      setResultReady(true);
+    });
+  };
 
   const reset = () => {
     setPhoto(null);
     setType('');
     setResultLink('');
     setResultReady(false);
-  }
-
+  };
 
   return (
     <div className="wrapper">
       <h1 className="heading">FLIP IMAGE</h1>
       <FileUpload photo={photo} setPhoto={val => setPhoto(val)} loading={loading} disabled={resultReady} />
-      {!resultReady ? (
+      {!resultReady
+        ? (
       <>
-        <select className="select" value={type} onChange={e => setType(e.target.value)}>
-            <option value="" disabled hidden>Choose flip mode...</option>
+        <label htmlFor="mode-select">Choose flip mode...</label>
+        <select id="mode-select" className="select" value={type} onChange={e => setType(e.target.value)}>
             <option value="vertical">Vertical</option>
             <option value="horizontal">Horizontal</option>
             <option value="both">Both</option>
         </select>
         <button className="button" onClick={getReversedImage}>Submit</button>
       </>
-      ) : (
+          )
+        : (
         <>
           <button className='button' onClick={reset}>submit another image</button>
           <a id="download" className="download button" href={resultLink} download="result"><span>download result</span><img src={downloadIcon} alt="" /></a>
         </>
-      )}
+          )}
       <div className="info">All rights reserved by me, 2022 ©</div>
   </div>
   );
--- frontend/src/components/FileUpload/index.js
+++ frontend/src/components/FileUpload/index.js
@@ -1,48 +1,51 @@
-import Dropzone from 'react-dropzone'
-import classnames from 'classnames'
-import UploadImage from '../../assets/uploadImage.png'
-import "./index.css"
+import React from 'react';
+import Dropzone from 'react-dropzone';
+import classnames from 'classnames';
+import UploadImage from '../../assets/uploadImage.png';
+import './index.css';
 
 const FileUpload = ({ photo, setPhoto, loading, disabled }) => {
-    const onDrop = acceptedFiles => {
-        const newPhoto = acceptedFiles[0];
-        newPhoto.preview = URL.createObjectURL(newPhoto);
-        setPhoto(newPhoto);
-    }
+  const onDrop = acceptedFiles => {
+    const newPhoto = acceptedFiles[0];
+    newPhoto.preview = URL.createObjectURL(newPhoto);
+    setPhoto(newPhoto);
+  };
 
-    const classes = classnames(
-        'drop-area', 
-        photo && 'drop-area--selected', 
-        loading && 'drop-area--loading',
-        disabled && 'drop-area--disabled'
-    )
-    console.log(loading);
-    return (
-        <Dropzone disabled={disabled} multiple={false} accept={{'image/*': ['.jpeg', '.png', '.jpg']}} onDrop={onDrop}>
-            {({getRootProps, getInputProps}) => (
+  const classes = classnames(
+    'drop-area',
+    photo && 'drop-area--selected',
+    loading && 'drop-area--loading',
+    disabled && 'drop-area--disabled'
+  );
+  console.log(loading);
+  return (
+        <Dropzone disabled={disabled} multiple={false} accept={{ 'image/*': ['.jpeg', '.png', '.jpg'] }} onDrop={onDrop}>
+            {({ getRootProps, getInputProps }) => (
                 <div {...getRootProps({
-                    className: classes
+                  className: classes
                 })}>
-                    {photo ? (
+                    {photo
+                      ? (
                         <figure className='selected-image'>
                             <img src={photo.preview} alt={photo.name} />
                             <figcaption>{photo.name}</figcaption>
                         </figure>
-                    ) : (
+                        )
+                      : (
                         <>
                             <input {...getInputProps()} />
                             <h1>Drop your image here...</h1>
-                            <img 
-                                className='upload-image' 
-                                src={UploadImage} 
+                            <img
+                                className='upload-image'
+                                src={UploadImage}
                                 alt="Upload"
                             />
                         </>
-                    )}
+                        )}
                 </div>
             )}
         </Dropzone>
-    );
-}
+  );
+};
 
 export default FileUpload;
